fe5d40c065338b680fb8d77ff68212ebbf83d313
Michał <mtarnowski90@hotmail.com>
2023-09-10 20:48:25 +0200

    main js. new functions added

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 const nav = document.querySelector('.nav')
 const navBtn = document.querySelector('.burger-btn')
 const allNavItems = document.querySelectorAll('.nav__item')
+const NavBars = document.querySelector('.burger-btn__bars')
+const allSections = document.querySelectorAll('.section') // zaznacz wszystkie elementy, które mają .section - dlatego ALL
+const footerYear = document.querySelector('.footer__year')
 
 const handleNav = () => {
 	nav.classList.toggle('nav--active')
+
+	NavBars.classList.remove('black-bars-color')
 
 	allNavItems.forEach(item => {
 		item.addEventListener('click', () => {
@@ -11,18 +16,39 @@ const handleNav = () => {
 		})
 	})
 
-	handleNavItemsAnimation();
+	handleNavItemsAnimation()
 }
-
-navBtn.addEventListener('click', handleNav)
 
 const handleNavItemsAnimation = () => {
 	let delayTime = 0
 
 	allNavItems.forEach(item => {
 		item.classList.toggle('nav-items-animation')
-		item.style.animationDelay = '.' + delayTime + 's';
+		item.style.animationDelay = '.' + delayTime + 's'
 		delayTime++
 	})
 }
 
+// Funckja, która obserwuje na jakiej sekci jesteśmy. Dzięku temu zmienia kolor navbara
+
+const handleObserver = () => {
+	const currentSection = window.scrollY
+	allSections.forEach(section => {
+		if (section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+			NavBars.classList.add('black-bars-color')
+		} else if (!section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+			// !section... -> jeżeli sekcja nie posiada klasy .contains = negacja
+			NavBars.classList.remove('black-bars-color')
+		}
+	})
+}
+
+// Funkcja wywołująca datę w footerze
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear()
+	footerYear.innerText = year
+}
+
+navBtn.addEventListener('click', handleNav)
+handleCurrentYear()
+window.addEventListener('scroll', handleObserver) // nadajemy nasłuch na całą przeglądarkę, w momencie kiedy zostanie wykryty scroll strony to wywołujemy funkcję
